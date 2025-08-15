f1929293fcbd35517c25610cfeb12e973c84182b
DEV-T-2025 <gustavo@gmail.com>
2025-08-15 14:20:11 -0300

    corrigir erro

--- webdev/settings.py
+++ webdev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statict')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'mycontacts/static'),]
+    BASE_DIR / 'mycontacts/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
